5d4f14a9ad970aa8dd8518eda4a21cbfb8b93891
catalin.candea <ecatalincandea@gmail.com>
2023-02-16 22:53:00 +0200

    [Identity-4] Add configuration for jwt, swagger and services

---
+++ Backend/TicketingSystem.Identity.API/TicketingSystem.Identity.API/Configuration/ServicesConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace TicketingSystem.Identity.API.Configuration
+{
+    public static class ServicesConfiguration
+    {
+
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddScopes();
+            services.AddHttpContextAccessor();
+            return services;
+        }
+        private static IServiceCollection AddScopes(this IServiceCollection services)
+        {
+            return services;
+        }
+        
+    }
+}
