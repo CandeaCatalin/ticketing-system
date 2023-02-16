af4d71c79c34998b3615b45d3e8bc56acd0967d9
catalin.candea <ecatalincandea@gmail.com>
2023-02-16 23:38:50 +0200

    [Identity-6] Refactor the project and solution structure to reflect what is necessary, docker-compose added

---
+++ Backend/Services/Identity/TicketingSystem.Identity.Domain/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TicketingSystem.Identity.Domain.Models
+{
+    public class User : IdentityUser
+    {
+        public DateTime CreatedAtTimeUtc { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
