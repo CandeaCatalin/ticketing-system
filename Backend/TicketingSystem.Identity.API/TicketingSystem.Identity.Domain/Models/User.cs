9c23e73fefd432a03747be89b9e433850baa4ee2
catalin.candea <ecatalincandea@gmail.com>
2023-02-16 22:19:04 +0200

    [Identity-5] Modify User model to extend IdentityUser and add DbContext

--- Backend/TicketingSystem.Identity.API/TicketingSystem.Identity.Domain/Models/User.cs
+++ Backend/TicketingSystem.Identity.API/TicketingSystem.Identity.Domain/Models/User.cs
@@ -1,17 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
+﻿using Microsoft.AspNetCore.Identity;
 
 namespace TicketingSystem.Identity.Domain.Models
 {
-    public class User
+    public class User : IdentityUser
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? Email { get; set; }
-        [JsonIgnore]
-        public string? Password { get; set; }
+        public DateTime CreatedAtTimeUtc { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
