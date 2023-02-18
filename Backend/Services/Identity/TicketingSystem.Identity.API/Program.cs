fc6466ac0b5455bd4a762cfe2c08d4d4d66dad8b
catalin.candea <ecatalincandea@gmail.com>
2023-02-18 13:13:06 +0200

    [Ticket-7] Add ticket api to the docker-compose, add a new instance for the  ticket database

--- Backend/Services/Identity/TicketingSystem.Identity.API/Program.cs
+++ Backend/Services/Identity/TicketingSystem.Identity.API/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 using TicketingSystem.Identity.API.Configuration;
 using TicketingSystem.Identity.DataAccess.Database;
@@ -10,7 +11,38 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
 
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                    }
+                });
+});
 var connectionString = builder.Configuration.GetConnectionString("identity");
 if (connectionString is not null)
 {
