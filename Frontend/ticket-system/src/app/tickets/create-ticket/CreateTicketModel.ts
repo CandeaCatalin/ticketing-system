08c462940a27abe3d8cf68bcba1b962c90acb0cb
catalin.candea <ecatalincandea@gmail.com>
2023-02-20 02:03:59 +0200

    [Frontend-9] Create the create ticket page and functionality

---
+++ Frontend/ticket-system/src/app/tickets/create-ticket/CreateTicketModel.ts
@@ -0,0 +1,10 @@
+export type CreateTicketModel={
+    subject:string;
+    description:string;
+    ticketTypeId: number;
+    customerName:string;
+    statusId:number;
+    priorityId:number;
+    userName:string;
+    serviceTypeId:number;
+}
---
+++ Frontend/ticket-system/src/app/tickets/create-ticket/create-ticket.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { TicketService } from '../ticket.service';
+import { CreateTicketModel } from './CreateTicketModel';
+
+@Component({
+  selector: 'app-create-ticket',
+  templateUrl: './create-ticket.component.html',
+  styleUrls: ['./create-ticket.component.scss']
+})
+export class CreateTicketComponent {
+  pageTitle = 'Create Ticket';
+  createdTicket : CreateTicketModel ={subject: "",description:"",customerName:"",ticketTypeId:0,statusId:0,priorityId:0,serviceTypeId:0,userName:localStorage.getItem("name") as string};
+  standardTicketProperties: any;
+  constructor(private ticketService:TicketService, private router:Router){}
+  createTicket(){
+    this.ticketService.createTicket(this.createdTicket).subscribe({
+      next: response => {
+        this.router.navigate(["home"]);
+      },
+      error: err => console.log(err)
+    });;
+  }
+  ngOnInit(){
+    this.ticketService.GetStandardProperties().subscribe({
+      next: standardProperties => {
+        this.standardTicketProperties = standardProperties;
+      },
+      error: err => console.log(err)
+    });
+  }
+}
